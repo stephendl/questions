c8f8acd9b7e833b7ee82f3be37957c850fcaf42c
Stephen Lock <stephen@eee.(none)>
2009-03-27 01:34:25 +0000

    added tests for vote counting

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :questions
+  
+  test "vote count with no votes" do
+    q = Question.new
+    assert_equal 0, q.vote_count
+  end
+  
+  test "vote count with one up vote" do
+    q = questions(:one)
+    assert_equal 1, q.vote_count
+  end
+  
+  test "vote count with one down vote" do
+    q = questions(:two)
+    assert_equal -1, q.vote_count
+  end
+  
+  test "vote count with one up and one down vote" do
+    q = questions(:evens)
+    assert_equal 0, q.vote_count
   end
 end
