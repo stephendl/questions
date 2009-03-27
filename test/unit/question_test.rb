4f8d764cb2cc9f85c800d5b400519332c435c1dd
Stephen Lock <stephen@eee.(none)>
2009-03-27 01:52:42 +0000

    no longer using fixtures for unit tests

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -4,22 +4,26 @@ class QuestionTest < ActiveSupport::TestCase
   fixtures :questions
   
   test "vote count with no votes" do
-    q = Question.new
+    q = Question.create
     assert_equal 0, q.vote_count
   end
   
   test "vote count with one up vote" do
-    q = questions(:one)
+  	q = Question.create
+    q.votes << Vote.create(:up => true)
     assert_equal 1, q.vote_count
   end
   
   test "vote count with one down vote" do
-    q = questions(:two)
+    q = Question.create
+    q.votes << Vote.create(:up => false)
     assert_equal -1, q.vote_count
   end
   
   test "vote count with one up and one down vote" do
-    q = questions(:evens)
+    q = Question.create
+    q.votes << Vote.create(:up => true)
+    q.votes << Vote.create(:up => false)
     assert_equal 0, q.vote_count
   end
 end
